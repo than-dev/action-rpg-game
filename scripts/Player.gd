ae86464421767c940e5f92595ce591d945fea536
than-dev <nathan.cotrim@gmail.com>
2022-12-12 02:34:42 -0300

    feat: apply smoothing in player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,25 +1,33 @@
+# player
 extends KinematicBody2D
 
+const ACCELERATION = 10;
+const MAX_SPEED = 100;
+const FRICTION = 10;
+
+# vector.zero = initial postion in scene vector (0, 0)
 var velocity = Vector2.ZERO;
 
-# set physics actions
-func _physics_process(_delta):
+# set physics actions and behavior (exec each frame)
+func _physics_process(delta):
+	
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");	
-	
-	if(input_vector.x < 0 && input_vector.y == 0):
-		$Sprite.flip_h = true;
-	elif(input_vector.x > 0 && input_vector.y == 0):
-		$Sprite.flip_h = false;		
-	
-	
-	if(input_vector != Vector2.ZERO):
-		velocity = input_vector;
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+	input_vector = input_vector.normalized();
+
+	# if there is movement input
+	if(input_vector != Vector2.ZERO):		
+		# setting movement based on ACCELERATION and input;
+		velocity += input_vector * ACCELERATION * delta;
+		
+		# limiting movement based on MAX_SPEED
+		velocity = velocity.limit_length(MAX_SPEED * delta);
 	else:
-		velocity = Vector2.ZERO;
-	
-	# move	
-	move_and_collide(velocity)
+		# setting desaceleration when stop to input any movement
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
+		
+	# move
+	move_and_collide(velocity);
 		
 	
