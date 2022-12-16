9a5e3522c317471987e2c61b60f15f2647fea2f3
than-dev <nathan.cotrim@gmail.com>
2022-12-16 15:39:00 -0300

    feat: attack animation + state machine

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,14 @@ const ACCELERATION = 500;
 const MAX_SPEED = 80;
 const FRICTION = 500;
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE; 
+
 # vector.zero = initial postion in scene vector (0, 0)
 var velocity = Vector2.ZERO;
 
@@ -17,8 +25,8 @@ onready var animation_state = animation_tree.get("parameters/playback");
 func _ready():
 	animation_tree.active = true
 	
-# set physics actions and behavior (exec each frame)
-func _physics_process(delta):
+	
+func move_state(delta: float):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
@@ -28,6 +36,7 @@ func _physics_process(delta):
 	if(input_vector != Vector2.ZERO):
 		animation_tree.set("parameters/idle/blend_position", input_vector);
 		animation_tree.set("parameters/run/blend_position", input_vector);
+		animation_tree.set("parameters/attack/blend_position", input_vector);
 		
 		animation_state.travel("run");
 				
@@ -40,5 +49,33 @@ func _physics_process(delta):
 		
 	# move
 	velocity = move_and_slide(velocity);
+	
+	if(Input.is_action_just_pressed("attack")):
+		velocity = Vector2.ZERO;
+		state = ATTACK;
+		
+
+func attack_state(delta: float):
+	animation_state.travel("attack");
+	
+func roll_state(delta: float):
+	print('rolling')
+	
+# set physics actions and behavior (exec each frame)
+func _physics_process(delta):
+		match state:
+			MOVE:
+				move_state(delta);
+			ROLL:
+				roll_state(delta);
+			ATTACK:
+				attack_state(delta);
+	
+
+func attack_animation_finished():
+	state = MOVE
+	
+	
+	
 		
 	
